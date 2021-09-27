fa0c5720e529942c5eebbc5d717132b6f7026641
Selenia <isamara.lopez@gmail.com>
2021-09-26 22:34:33 -0600

    Panel de control - 74

--- sistema/index.php
+++ sistema/index.php
@@ -10,9 +10,144 @@ session_start();
 	<title>Sistema Ventas</title>
 </head>
 <body>
+
 <?php  include "includes/header.php"; ?>
 	<section id="container">
-		<h1>Bienvenido al sistema</h1>
+
+		<div class="divContainer">
+			<div>
+            <h1 class="titlePanelControl">Panel de control</h1>
+</div>
+
+<div class="dashboard">
+	<a href="lista_usuarios.php">
+		<i class="fas fa-users"> </i>
+		<P>
+		   <strong> Usuarios</strong></br>
+		   <span>40 </span>
+</p>
+</a>
+<a href="lista_clientes.php">
+		<i class="fas fa-user"> </i>
+		<P>
+		   <strong> Clientes</strong></br>
+		   <span>10000 </span>
+</p>
+</a>
+<a href="lista_proveedor.php">
+		<i class="far fa-building"> </i>
+		<P>
+		   <strong> Proveedores</strong></br>
+		   <span>200 </span>
+</p>
+</a>
+<a href="lista_producto.php">
+		<i class="fas fa-cubes"> </i>
+		<P>
+		   <strong> Productos</strong></br>
+		   <span>2000 </span>
+</p>
+</a>
+<a href="ventas.php">
+		<i class="fas fa-file-alt"> </i>
+		<P>
+		   <strong> Ventas</strong></br>
+		   <span>500 </span>
+</p>
+</a>
+</div>
+		</div>
+
+
+	<div class="divInfoSistema">
+		<div>
+			<h1 class="titlePanelControl">Configuración</h1>
+			
+</div> 
+        <div class="containerPerfil">
+			<div class="containerDataUser">
+				<div class="logoUser">
+					<img src="img/logoUser.png">
+</div>
+        <div class="divDataUser">
+			<h4> Información Personal </h4>
+			<div>
+				<label> Nombre: </label> <span> Selenia </span>
+            </div>
+           
+			<div>
+				<label> Correo: </label> <span> Selenia </span>
+             </div>
+			 <h4> Datos usuario </h4>
+			 <div>
+				 <label>Rol:</label> <span> selenia </span>
+            </div>
+			<div>
+			<label>Usuario:</label> <span> admin </span>
+            </div>
+			<h4>Cambiar contraseña </h4>
+			<form action="" method="post" name="frmChangePass" id="frmChangePass">
+				<div>
+					<input type="password" name="txtPassUser" id="textPassUser" placeholder="Contraseña actual" required>
+                </div>
+				<div>
+					<input type="password" name="txtNewPassUser" id="textNewPassUser" placeholder="Nueva contraseña" required>
+                </div>
+				<div>
+					<button type="submit" class="btn_save  btnChangePass"> <i class="fas fa-key"></i>Cambiar contraseña </button>
+                </div>
+
+     </form>			
+		</div>
+     </div>
+
+<div class="containerDataEmpresa">
+
+	<div class="logoEmpresa">
+	     <img src="img/logoEmpresa.png">
+    </div>
+	<h4> Datos del negocio <h4>
+
+
+
+   <form action =""	 method= "post" name="frmEmpresa" id="frmempresa">
+	   <input type="hidden" name="action" value ="udpateDataEmpresa"> 
+
+	   <div>
+		   <label style="color:black;"> Ruc: </label><input type="text" name="txtNit" id="txtNit" placeholder="Ruc del negocio" value=""required>
+       </div>
+	   <div>
+	   <label style="color:black;"> Nombre: </label><input type="text" name="txtNombre" id="txtNombre" placeholder="Nombre del negocio" value=""required>
+       </div>
+	   <div>
+	   <label style="color:black;"> Razon social: </label><input type="text" name="txtRSocial" id="txtRSocial" placeholder="Razon social" value=""required>
+       </div>
+	   <div>
+	   <label style="color:black;"> Teléfono: </label><input type="text" name="txtTelEmpresa" id="txtTelEmpresa" placeholder="Teléfono del negocio" value=""required>
+       </div>
+	   <div>
+	   <label style="color:black;"> Correo electrónico: </label><input type="email" name="txtEmailEmpresa" id="txtEmailEmpresa" placeholder="Correo del negocio" value=""required>
+       </div>
+	   <div>
+	   <label style="color:black;"> Dirección: </label><input type="text" name="txtDirEmpresa" id="txtDirEmpresa" placeholder="Dirección del negocio" value=""required>
+       </div>
+	   <div>
+	   <label style="color:black;"> IVA (%): </label><input type="text" name="txtIva" id="txtIva" placeholder="Impuesto al valor agregado
+	   (IVA) " value=""required>
+       </div>
+	   
+	   <div class="alertFormEmpresa" style="display: none;"> </div>
+       <div>
+		   <button type="submit" class="btn_save btnChangePass"> <i class="far fa-save fa-lg"></i> Guardar datos </button>
+         
+           </div>
+    </form>
+
+</div>
+
+
+</div>
+</div>
 	</section>
 
 	<?php  include "includes/footer.php"; ?>
